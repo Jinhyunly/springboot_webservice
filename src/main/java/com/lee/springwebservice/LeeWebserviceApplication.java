243142cfe5913f61b269f8da3aa823f8e205931d
lee <dbdbddipp@gmail.com>
2020-10-19 10:31:53 +0900

    create project

---
+++ src/main/java/com/lee/springwebservice/LeeWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lee.springwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lee/springwebservice/LeeWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.springwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeeWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
